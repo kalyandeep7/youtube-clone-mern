c752d1bd0b61d9c4b0c7dc06718faf1b14810145
kalyandeep7 <kalyandeep2001@gmail.com>
2026-04-13 11:31:37 +0530

    feat(backend): add User model

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: '',
+    },
+    channels: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Channel',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model('User', userSchema);
